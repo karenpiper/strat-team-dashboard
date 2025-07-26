4a6fed659367856e1ca59b72c4c5dcc9045f51d8
karenpiper <karen.piper@codeandtheory.com>
2025-07-26 11:48:41 -0700

    Update airtable.ts

--- lib/airtable.ts
+++ lib/airtable.ts
@@ -1,12 +1,11 @@
 import Airtable from "airtable"
 
-const base = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY }).base(
-  process.env.AIRTABLE_BASE_ID!
+const base = new Airtable({ apiKey: process.env.NEXT_PUBLIC_AIRTABLE_API_KEY }).base(
+  process.env.NEXT_PUBLIC_AIRTABLE_BASE_ID!
 )
 
 export const getAirtableRecords = async (tableName: string) => {
   const records: any[] = []
-
   return new Promise((resolve, reject) => {
     base(tableName)
       .select({ view: "Grid view" })
